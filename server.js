470871cc6c9abea50b297f66a6fc366363068fe7
xlabcba <xie.li@husky.neu.edu>
2016-03-30 20:39:22 -0400

    Assignment 4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var session       = require('express-session');
 var mongoose      = require('mongoose');
 
 // create a default connection string
-var connectionString = 'mongodb://127.0.0.1:27017/WebDevSpring2016';
+var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
 // use remote connection string
 // if running in remote server
@@ -20,6 +20,9 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 
+// connect to the database
+var db = mongoose.connect(connectionString);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,12 +30,10 @@ app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
-
-// connect to the database
-var db = mongoose.connect(connectionString);
 
 require("./public/assignment/server/app.js")(app, db, mongoose);
 require("./public/project/server/app.js")(app);
