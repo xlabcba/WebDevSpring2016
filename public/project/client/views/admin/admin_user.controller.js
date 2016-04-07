36a9123b1962fe64177383fa6e9deef1c5b16f78
xlabcba <xie.li@husky.neu.edu>
2016-04-07 00:28:03 -0400

    Assignment 3 Prototype

--- public/project/client/views/admin/admin_user.controller.js
+++ public/project/client/views/admin/admin_user.controller.js
@@ -8,8 +8,104 @@
         .module("RecipeApp")
         .controller("AdminUserController", AdminUserController);
 
-    function AdminUserController($rootScope, $scope, $location)
+    function AdminUserController($rootScope, $scope, $location, UserService)
     {
+        var vm = this;
 
+        vm.deleteUser = deleteUser;
+        vm.updateUser = updateUser;
+        vm.setModalUser = setModalUser;
+        vm.cleanModalUser = cleanModalUser;
+
+        function init() {
+            vm.checkboxes = { admin: false, user: false };
+            vm.modalUser = {};
+            vm.users = [];
+            UserService
+                .findAllUsers()
+                .then(function(response){
+                    console.log(response.data);
+                    vm.users = response.data;
+                });
+        }
+        init();
+
+        function deleteUser(user) {
+            UserService
+                .deleteUserById(user._id)
+                .then(function(response){
+                    init()
+                });
+        }
+
+        function updateUser(user) {
+
+            if(!user.password) {
+                alert("password cannot be empty");
+            }
+
+            user.roles = [];
+            if (vm.checkboxes.admin) {
+                user.roles.push("admin");
+            }
+            if (vm.checkboxes.user) {
+                user.roles.push("user");
+            }
+
+            var newUser = {
+                _id: user._id,
+                photo: user.photo,
+                gender: user.gender,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username,
+                password: user.password,
+                email: user.email,
+                birthday: user.birthday,
+                follow: user.follow,
+                followBy: user.followBy,
+                like: user.like,
+                intro: user.intro,
+                roles: user.roles
+            };
+
+            UserService
+                .updateUser(user._id, newUser)
+                .then(function(response){
+                    console.log(response.data);
+                    init();
+                });
+        }
+
+        function setModalUser(user) {
+            vm.modalUser = {
+                _id: user._id,
+                photo: user.photo,
+                gender: user.gender,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username,
+                password: user.password,
+                email: user.email,
+                birthday: user.birthday,
+                follow: user.follow,
+                followBy: user.followBy,
+                like: user.like,
+                intro: user.intro,
+                roles: user.roles
+            };
+
+            if(vm.modalUser.roles.indexOf("admin") >= 0) {
+                vm.checkboxes.admin = true;
+            }
+            if(vm.modalUser.roles.indexOf("user") >= 0) {
+                vm.checkboxes.user = true;
+            }
+        }
+
+        function cleanModalUser() {
+            vm.modalUser = {};
+            vm.checkboxes = { admin: false, user: false };
+        }
     }
 })();
